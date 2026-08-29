d0a852aca0bdfcc9e8f36fbcb90fd5b1efa0cd89
Ravinder Reddy <developer.ravinder.reddy@gmail.com>
2026-08-29 17:18:45 +0530

    feat: add more data types

--- learn_01_core/src/bin/learn_01_03_data_types.rs
+++ learn_01_core/src/bin/learn_01_03_data_types.rs
@@ -3,13 +3,37 @@
 // Unlike JS's single `number`, you pick size/signedness (like Java).
 fn main() {
     // Scalars: integers (iN/uN), floats (f32/f64), bool, char.
-    let signed: i32 = -42;
-    let unsigned: u8 = 255; // 0..=255
-    let big: i64 = 9_000_000_000; // underscores are separators
-    let pi: f64 = 3.14159;
+    let unsigned8: u8 = 255; // 0..=255
+    let unsigned16: u16 = 1245;
+    let unsigned32: u32 = 11233434;
+    let unsigned64: u64 = 123455;
+    let unsigned128: u128 = 123455;
+    let unsigned: usize = 12343556; // size depends on platform
+
+
+    let signed8: i8 = -42;
+    let signed16: i16 = -142;
+    let signed32: i32 = -242;
+    let signed64: i64 = 9_000_000_000; // underscores are separators
+    let signed128: i128 = 79_9_000_000_000; // underscores are separators
+    let signed: isize = 9727727;// size depends on platform
+
+    let float32: f32 = 3.14159;
+    let float64: f64 = 3.14159;
+
+    let is_present: bool = true; 
+    let is_lazy: bool = false;
+
     let letter: char = 'R'; // 4-byte Unicode scalar, not a byte
     let crab: char = '🦀';
-    println!("{signed} {unsigned} {big} {pi} {letter} {crab}");
+
+    println!("{unsigned8}, {unsigned16}, {unsigned32}, {unsigned64}, {unsigned128}, {unsigned}");
+    println!("{signed8}, {signed16}, {signed32}, {signed64}, {signed128}, {signed}");
+    println!("{float32}, {float64}");
+
+    println!("{is_present},{is_lazy}");
+
+    println!("{signed32} {unsigned8} {signed64} {float64} {letter} {crab}");
 
     // Tuple: fixed-length, mixed types.
     let person = ("Ravinder", 30, true);
